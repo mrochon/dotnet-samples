0da603069ec135a8d26e1933b4e19320eb7f694e
MariusR <mrochon@microsoft.com>
2023-09-08 14:41:35 -0700

    Init

---
+++ blazor/Data/AzureService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.Identity.Web;
+
+namespace blazor.Data
+{
+    [Authorize]
+    public class AzureService
+    {
+        private readonly ILogger<GraphService> _logger;
+        private readonly IDownstreamWebApi _downstreamWebApi;
+        public AzureService(ILogger<GraphService> logger,
+                              IDownstreamWebApi downstreamWebApi)
+        {
+            _logger = logger;
+            _downstreamWebApi = downstreamWebApi;
+        }
+        [AuthorizeForScopes(ScopeKeySection = "AzureApi:Scopes")]
+        public async Task<string> GetRolesAsync()
+        {
+            using var response = await _downstreamWebApi.CallWebApiForUserAsync("AzureApi").ConfigureAwait(false);
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
---
+++ blazor/Data/GraphService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web;
+
+namespace blazor.Data
+{
+    [Authorize]
+    public class GraphService
+    {
+        private readonly ILogger<GraphService> _logger;
+        private readonly IDownstreamWebApi _downstreamWebApi;
+        public GraphService(ILogger<GraphService> logger,
+                              IDownstreamWebApi downstreamWebApi)
+        {
+            _logger = logger;
+            _downstreamWebApi = downstreamWebApi;
+        }
+        [AuthorizeForScopes(ScopeKeySection = "GraphApi:Scopes")]
+        public async Task<string> GetUserAsync()
+        {
+            // Call GraphApi to get user information
+            using var response = await _downstreamWebApi.CallWebApiForUserAsync("GraphApi").ConfigureAwait(false);
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
